675a0078414ab7e930b36a14172bea519ef3b846
Janiskanth <141775307+Janiskanth@users.noreply.github.com>
2024-04-07 02:58:39 +0530

    add file

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('#home').click(function() {
+        $('#body').load("index.html");
+    });
+
+    $('#about').click(function() {
+        $('#body').load("./pages/component.html");
+    });
+
+    $('#Service').click(function() {
+        $('#body').load("./pages/component.html");
+    });
+
+    $('#contact').click(function() {
+        $('#body').load("./pages/component.html");
+    });
+
+    $('#career').click(function() {
+        $('#body').load("./pages/component.html");
+    });
+});
